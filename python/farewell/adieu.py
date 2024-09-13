0269a0ceb632c8a3e04f1b2b66a77e113db810ea
Rocha <arkhanticsbr@outlook.com>
2024-09-13 12:44:02 -0300

    test

---
+++ python/farewell/adieu.py
@@ -0,0 +1,23 @@
+import sys
+import inflect
+
+def main():
+    p = inflect.engine()
+    
+    names = []
+    try:
+        while True:
+            name = input("name: ").strip()
+            if name:
+                names.append(name)
+    except EOFError:
+        pass
+    
+    if not names:
+        return
+    
+    formatted_names = p.join(names)
+    print(f"Adieu, adieu, to {formatted_names}")
+
+if __name__ == "__main__":
+    main()
